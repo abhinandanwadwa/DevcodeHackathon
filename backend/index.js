1f81aa19bd3aaaa3a50c3db4c0b4b3e55bc3422f
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2023-02-08 15:31:08 +0530

    added ssl certificate

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 // const connectToMongo = require('./db');
 const app = express();
 
@@ -20,7 +22,12 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/project', require('./routes/project'));
 
-// Starting The Server
-app.listen(PORT, () => {
-    console.log(`The server is running on the PORT: ${PORT}`);
-});
+const options = {
+    key: fs.readFileSync('ssl/privkey.pem'),
+    cert: fs.readFileSync('ssl/cert.pem')
+  };
+  
+  https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('Hello World!\n');
+  }).listen(8181);
