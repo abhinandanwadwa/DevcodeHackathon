fec3442d194ad1a4328077e3d8a741363a61270d
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2023-02-11 22:06:50 +0530

    normalized the server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
 // const connectToMongo = require('./db');
 const app = express();
 
@@ -22,12 +20,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/project', require('./routes/project'));
 
-const options = {
-    key: fs.readFileSync('ssl/privkey.pem'),
-    cert: fs.readFileSync('ssl/cert.pem')
-  };
-  
-  https.createServer(options, (req, res) => {
-    res.writeHead(200);
-    res.end('Hello World!\n');
-  }).listen(8181);
+// Starting The Server
+app.listen(PORT, () => {
+    console.log(`The server is running on the PORT: ${PORT}`);
+});
